ae3fa494fda54c3e6f97e9bd314429ed353885f8
Lera <vbaptidanova@gmail.com>
2022-09-08 20:47:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+let element = document.getElementById('app');
+
+function changeColor(colorText = 'red') {
+element.style.color = colorText;    
+element.innerHTML = "<h1>NEW TEXT </h1>";
+}
